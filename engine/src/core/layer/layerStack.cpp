2b756bee1280fdb0c61012bdd859d38d8b63d3ca
PedroGaya <pedrohenriquegaya@gmail.com>
2024-07-15 15:54:22 -0300

    layers renaming

--- engine/src/core/layer/layerStack.cpp
+++ engine/src/core/layer/layerStack.cpp
@@ -4,35 +4,35 @@
 namespace JC2D {
 
     LayerStack::~LayerStack() {
-        for (Layer* layer : m_Layers) {
+        for (Layer* layer : m_layers) {
             layer->onDetach();
             delete layer;
         }
     }
 
-    void LayerStack::PushLayer(Layer* layer) {
-        m_Layers.emplace(m_Layers.begin() + m_LayerInsertIndex, layer);
-        m_LayerInsertIndex++;
+    void LayerStack::pushLayer(Layer* layer) {
+        m_layers.emplace(m_layers.begin() + m_layerInsertIndex, layer);
+        m_layerInsertIndex++;
     }
 
-    void LayerStack::PushOverlay(Layer* overlay) {
-        m_Layers.emplace_back(overlay);
+    void LayerStack::pushOverlay(Layer* overlay) {
+        m_layers.emplace_back(overlay);
     }
 
-    void LayerStack::PopLayer(Layer* layer) {
-        auto it = std::find(m_Layers.begin(), m_Layers.begin() + m_LayerInsertIndex, layer);
-        if (it != m_Layers.begin() + m_LayerInsertIndex) {
+    void LayerStack::popLayer(Layer* layer) {
+        auto it = std::find(m_layers.begin(), m_layers.begin() + m_layerInsertIndex, layer);
+        if (it != m_layers.begin() + m_layerInsertIndex) {
             layer->onDetach();
-            m_Layers.erase(it);
-            m_LayerInsertIndex--;
+            m_layers.erase(it);
+            m_layerInsertIndex--;
         }
     }
 
-    void LayerStack::PopOverlay(Layer* overlay) {
-        auto it = std::find(m_Layers.begin() + m_LayerInsertIndex, m_Layers.end(), overlay);
-        if (it != m_Layers.end()) {
+    void LayerStack::popOverlay(Layer* overlay) {
+        auto it = std::find(m_layers.begin() + m_layerInsertIndex, m_layers.end(), overlay);
+        if (it != m_layers.end()) {
             overlay->onDetach();
-            m_Layers.erase(it);
+            m_layers.erase(it);
         }
     }
 

f50a54302617388c8a4ee3e0c2c19b67f39eb232
PedroGaya <pedrohenriquegaya@gmail.com>
2024-07-25 19:06:54 -0300

    unsigned int

--- engine/src/core/render/shaders/shader.h
+++ engine/src/core/render/shaders/shader.h
@@ -11,7 +11,7 @@ namespace JC2D {
         virtual unsigned int createProgram();
 
         inline void use() { glUseProgram(m_id); }
-        inline int getId() const { return m_id; }
+        inline unsigned int getId() const { return m_id; }
 
         inline void setUniform4f(const std::string &name, float v1, float v2, float v3, float v4) const {
             glUniform4f(glGetUniformLocation(m_id, name.c_str()), v1, v2, v3, v4);

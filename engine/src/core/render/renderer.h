9c86163e994954bec9573ad2c0826a522734f7fa
PedroGaya <pedrohenriquegaya@gmail.com>
2024-07-28 19:23:22 -0300

    static draw calls

--- engine/src/core/render/renderer.h
+++ engine/src/core/render/renderer.h
@@ -3,9 +3,9 @@
 
 namespace JC2D {
     class JC2D_API Renderer {
-        void clear();
-        void setClearColor(const glm::vec4& color);
+        inline static void clear();
+        inline static void setClearColor(const glm::vec4& color);
 
-        void drawIndexed(const std::shared_ptr<VertexArray>& vertexArray);
+        inline static void drawIndexed(const std::shared_ptr<VertexArray>& vertexArray);
     };
 }  // namespace JC2D
